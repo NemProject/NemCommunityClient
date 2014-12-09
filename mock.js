0ae7a5391190cbd2d3d19d8439d607308b1ef216
Paulo Cesar <email@pocesar.e4ward.com>
2014-12-09 03:09:01 -0200

    add TEST_MOCK environment variable

--- mock.js
+++ mock.js
@@ -4,34 +4,53 @@ var
     path = require('path'),
     exec = require('child_process').spawn,
     seleniumStandalone = require('selenium-standalone'),
-    seleniunLogs = require('fs').createWriteStream('./selenium-logs.txt')
+    seleniunLogs = require('fs').createWriteStream('./selenium-logs.txt'),
+    handleMocks = mock('./tests/fixtures/http/'),
+    webapp = path.join('nem-client-api','src','main','webapp'),
+    serveStatic = require('serve-static')(webapp)
     ;
 
-server = http.createServer(mock('./tests/fixtures/http/')).listen(9001, function(){
-    var selenium = seleniumStandalone({
-        stdio: null
-    }, {
-        port: 4444,
-        host: '127.0.0.1'
+server = http.createServer(function(req, res){
+    serveStatic(req, res, function(){
+        handleMocks(req, res);
+    });
+}).listen(8989, function(){
+    console.log('Started mock server on http://127.0.0.1:8989');
+
+    process.on('SIGINT', function(){
+        try {
+            server.close();
+            process.exit();
+        } catch (e) { }
     });
 
-    selenium.stderr.pipe(seleniunLogs);
+    if (!process.env.TEST_MOCK) {
 
-    var intern = exec('node', [require.resolve('intern/bin/intern-runner'), 'config=tests/intern'], {
-        cwd: process.cwd(),
-        env: process.env,
-        stdio: null
-    });
+        var selenium = seleniumStandalone({
+            stdio: null
+        }, {
+            port: 4444,
+            host: '127.0.0.1'
+        });
 
-    intern.on('close', function(code){
-        server.close();
-        process.exit(code);
-    });
+        selenium.stderr.pipe(seleniunLogs);
 
-    intern.stderr.on('data', function(data){
-        console.log(data.toString());
-    });
+        var intern = exec('node', [require.resolve('intern/bin/intern-runner'), 'config=tests/intern'], {
+            cwd: process.cwd(),
+            env: process.env,
+            stdio: null
+        });
 
-    intern.stdout.pipe(process.stdout);
+        intern.on('close', function(code){
+            server.close();
+            process.exit(code);
+        });
 
+        intern.stderr.on('data', function(data){
+            console.log(data.toString());
+        });
+
+        intern.stdout.pipe(process.stdout);
+
+    }
 });
